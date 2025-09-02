8643f2e9e1d6d0cf230b0b468a969a47614f6d4a
arpitkatiyar108 <arpitkatiyar108@gmail.com>
2025-09-02 15:49:15 +0530

    Complete-video-8: Django Templates

--- Textutils/Textutils/views.py
+++ Textutils/Textutils/views.py
@@ -4,20 +4,34 @@ from django.shortcuts import render
 
 
 def home(request) :
-    return HttpResponse('''HOME!''')
+    return render(request, 'home.html')
+    # return HttpResponse('''HOME!
+    # <h3><a href="/">Back</a></h3>
+    # ''')
 
 def removepunc(request) :
-    return HttpResponse('''REMOVE!''')
+    return HttpResponse('''REMOVE!
+    <h3><a href="/">Back</a></h3>
+    ''')
 
 def capfirst(request) :
-    return HttpResponse('''CAP FIRST''')
+    return HttpResponse('''CAP FIRST
+    <h3><a href="/">Back</a></h3>
+    ''')
 
 def newlineremove(request) :
-    return HttpResponse('''NEWLINE REMOVE!''')
+    return HttpResponse('''NEWLINE REMOVE!
+    <h3><a href="/">Back</a></h3>
+    ''')
 
 def spaceremove(request) :
-    return HttpResponse('''SPACE REMOVE!''')
+    return HttpResponse('''SPACE REMOVE!
+    <h3><a href="/">Back</a></h3>
+    ''')
 
 def charcount(request) :
-    return HttpResponse('''CHARACTER COUNT!''')
+    return HttpResponse('''
+    CHARACTER COUNT!
+    <h3><a href="/">Back</a></h3>
+    ''')
 
